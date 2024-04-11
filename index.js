8c28c8c3fad91aef9138e80c91f9f8095698ec99
relmelegy <77593012+relmelegy@users.noreply.github.com>
2024-04-11 16:52:51 -0400

    commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require('inquirer');
+const qr = require('qr-image');
+const fs = require('fs');
+
+async function generateQRFromURL() {
+  // Step 1: Get user input for URL
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'url',
+      message: 'Enter the URL to generate a QR code:',
+      validate: function(value) {
+        var pass = value.match(
+          /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+        );
+        if (pass) {
+          return true;
+        }
+
+        return 'Please enter a valid URL';
+      },
+    },
+  ]);
+
+  const { url } = answers;
+
+  // Step 2: Generate QR code
+  const qrSVG = qr.image(url, { type: 'png' });
+  qrSVG.pipe(fs.createWriteStream('QRCode.png'));
+  console.log('QR code generated successfully: QRCode.png');
+
+  // Step 3: Save user input to a text file
+  fs.writeFileSync('userInput.txt', url);
+  console.log('User input saved to userInput.txt');
+}
+
+generateQRFromURL().catch(console.error);
